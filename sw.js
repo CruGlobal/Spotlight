a5b2f9d41fac5f58dc86f9f1a5f7d41130fd1ae1
Carl Hempel <cshempel@gmail.com>
2023-05-04 11:23:12 -0400

    Better caching

--- sw.js
+++ sw.js
@@ -1,63 +1,91 @@
-var cacheName = 'spotlight-1.0';
-var prefetchedURLs = [
-  'icon.png',
-  'icon512.png',
-  'apple-touch-icon.png',
+/* global caches, fetch, self */
+const CACHE_NAME = 'spotlight-0.2'
+const CACHED_URLS = [
   'browserconfig.xml',
+  'favicon.ico',
+  'a2hs.png',
+  'apple-touch-icon.png',
   'favicon-16x16.png',
   'favicon-32x32.png',
-  'favicon.ico',
+  'genericQ.png',
+  'holy_spirit_presentations.png',
   'icon.png',
   'icon512.png',
   'mstile-150x150.png',
+  'personal_decisions.png',
+  'personal_evangelism.png',
+  'Screenshot1.png',
+  'Screenshot2.png',
+  'spiritual_conversations.png',
+  'teamQ.png',
   'safari-pinned-tab.svg',
+  'share-apple.svg',
+  'analytics.js',
+  'lib.js',
+  'masking-input.js',
   'party.min.js',
-  'masking-input.js'
-  /*'lib.js',
   'sw.js',
   'style.css',
   'index.html',
-  'gospel.png',
-  'spirit.png',
-  'decision.png',
-  'manifest.webmanifest',*/
-];
-const l = console.log
+  'manifest.webmanifest'
+]
 
-self.addEventListener('install', function(event) {
-  l('install evt')
+// Open cache on install.
+self.addEventListener('install', event => {
+  event.waitUntil(async function () {
+    const cache = await caches.open(CACHE_NAME)
 
-  event.waitUntil(
-    caches.open(cacheName).then((cache) => {
+    await cache.addAll(CACHED_URLS)
+  }())
+})
 
-      return Promise.all(prefetchedURLs.map((url) => {
-        return fetch(url).then(res => {
-          if (res.status >= 400) throw Error('request failed: ' + url + ' failed with status: ' + res.statusText)
-          return cache.put(url, res)
-        })
-      }))
+// Cache and update with stale-while-revalidate policy.
+self.addEventListener('fetch', event => {
+  
+  if(event.request.url.match('^.*script.google.com/macros/.*$')) {
+    return false;
+  }
 
-    }).catch((err) => {
-      //...
-      l(err
-)    })
-    //...
-  )
-});
+  const { request } = event
 
+  // Prevent Chrome Developer Tools error:
+  // Failed to execute 'fetch' on 'ServiceWorkerGlobalScope': 'only-if-cached' can be set only with 'same-origin' mode
+  //
+  // See also https://stackoverflow.com/a/49719964/1217468
+  if (request.cache === 'only-if-cached' && request.mode !== 'same-origin') {
+    return
+  }
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(res) {
-      if (res) {
-        l('Resource found in Cache Storage')
-        return res;
-      }
+  event.respondWith(async function () {
+    const cache = await caches.open(CACHE_NAME)
 
-      return fetch(event.request).then(function(res) {
-        return res;
-      }).catch(function(err) {
-        l(err);
-      });
-    }));
-});
+    const cachedResponsePromise = await cache.match(request)
+    const networkResponsePromise = fetch(request)
+
+    if (request.url.startsWith(self.location.origin)) {
+      event.waitUntil(async function () {
+        const networkResponse = await networkResponsePromise
+
+        await cache.put(request, networkResponse.clone())
+      }())
+    }
+
+    return cachedResponsePromise || networkResponsePromise
+  }())
+})
+
+// Clean up caches other than current.
+self.addEventListener('activate', event => {
+  event.waitUntil(async function () {
+    const cacheNames = await caches.keys()
+
+    await Promise.all(
+      cacheNames.filter((cacheName) => {
+        const deleteThisCache = cacheName !== CACHE_NAME
+        console.log('deleting', cacheName);
+
+        return deleteThisCache
+      }).map(cacheName => caches.delete(cacheName))
+    )
+  }())
+})
